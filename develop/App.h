be4207932f4209d37328c5e56a89f1749357707d
Nikita <nikitalevuskin41668@gmail.com>
2023-03-14 14:13:53 +0300

    add App.h

--- develop/App.h
+++ develop/App.h
@@ -1,1 +1,10 @@
 #include <iostream>
+#include <string>
+#include <limits>
+
+int figure_location(int figure);
+
+int x_white_king, y_white_king, x_black_elephant, y_black_elephant, x_black_rook, y_black_rook;
+
+int x_figure, y_figure;
+
