a2689280775c69e98a0748e219934d784426fb6d
Nikita <nikitalevuskin41668@gmail.com>
2023-03-14 14:11:04 +0300

    some change

--- develop/lib.cpp
+++ develop/lib.cpp
@@ -1,18 +1,22 @@
-#include <iostream>
+#include "lib.h"
 using namespace std;
 
-void check_elephant (int x1, y1, x2, y2){
-    if ( ads(x1-x2) == abs(y1-y2) ){
-        cout << "Шах от слона";
+int check_elephant (int x1, int y1, int x2, int y2){
+    if ( x1+y1 == x2+y2 || x1-y1 == x2-y2 ){
+        cout << "elephant good";
     }
+    else {
+        cout << "not elephant good";
+    }
+    return 0;
 }
 
-void check_rook (int x1, y1, x2, y2){
+int check_rook (int x1, int y1, int x2, int y2){
     if ( x1 == x2 || y1 == y2){
-        cout << "Шах от ладьи";
+        cout << "rook good";
     }
-}
-
-int main(){
-    return(0);
+    else {
+        cout << "not rook good";
+    }
+    return 0;
 }
---
+++ develop/lib.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+
+int check_elephant(int x1, int y1, int x2, int y2);
+
+int check_rook(int x1, int y1, int x2, int y2);
