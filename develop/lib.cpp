a0586a61c4f7c1c8f1ca45c9bc031b4398955f72
Nikita <nikitalevuskin41668@gmail.com>
2023-03-17 19:41:56 +0300

    fix bug out

--- develop/lib.cpp
+++ develop/lib.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int check_elephant (int x1, int y1, int x2, int y2){
     if ( x1+y1 == x2+y2 || x1-y1 == x2-y2 ){
-        cout << "Ўах от слона";
+        cout << "Ўах от слона\n";
     }
     else return 1;
     return 0;
@@ -11,7 +11,7 @@ int check_elephant (int x1, int y1, int x2, int y2){
 
 int check_rook (int x1, int y1, int x2, int y2){
     if (x1 == x2 || y1 == y2) {
-        cout << "Ўах от ладьи";
+        cout << "Ўах от ладьи\n";
     }
     else return 1;
     return 0;
