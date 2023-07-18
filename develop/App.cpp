a5b4e36a37566e7f9ac92268db84b94b95459ebb
Nikita <nikitalevuskin41668@gmail.com>
2023-07-19 00:06:24 +0300

    add overlapped bishop

--- develop/App.cpp
+++ develop/App.cpp
@@ -8,6 +8,56 @@ using namespace std;
 string word_list[8] = {"a", "b", "c", "d", "e", "f", "g", "h"};
 int number_list[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+int overlapped_figures(string figures) {
+	if (figures == "elephant") {
+		if (y_black_elephant > y_white_king) {
+			if (x_black_elephant > x_white_king) {
+				int y = x_black_elephant + 1;
+				for (int i = y_black_elephant; i >= y_white_king; i--) {
+					y -= 1;
+					if (i == y_black_rook && y == x_black_rook) {
+						return 1;
+					}
+				}
+			}
+			else {
+				int y = x_black_elephant - 1;
+				for (int i = y_black_elephant; i >= y_white_king; i--) {
+					y += 1;
+					if (i == y_black_rook && y == x_black_rook) {
+						return 1;
+					}
+				}
+			}
+		}
+		else {
+			if (x_black_elephant > x_white_king) {
+				int y = x_black_elephant + 1;
+				for (int i = y_black_elephant; i <= y_white_king; i++) {
+					y -= 1;
+					if (i == y_black_rook && y == x_black_rook) {
+						return 1;
+					}
+				}
+			}
+			else {
+				int y = x_black_elephant - 1;
+				for (int i = y_black_elephant; i <= y_white_king; i++) {
+					y += 1;
+					if (i == y_black_rook && y == x_black_rook) {
+						return 1;
+					}
+				}
+			}
+		}
+		cout << "шах от слона";
+		return 0;
+	}
+	if (figures == "rook") {
+		return 0
+	}
+}
+
 std::pair<int, int> figure_location(string figure) {
 	cin.clear();
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -36,7 +86,7 @@ std::pair<int, int> figure_location(string figure) {
 	figure_location(figure);
 }
 
-int main()	{
+int main() {
 	setlocale(LC_ALL, "ru");
 	cout << "Нажмите любую кнопку чтобы начать";
 
@@ -52,12 +102,25 @@ int main()	{
 	x_black_rook = c.first;
 	y_black_rook = c.second;
 
-	int f = check_elephant(x_white_king, y_white_king, x_black_elephant, y_black_elephant);
-	int g = check_rook(x_white_king, y_white_king, x_black_rook, y_black_rook);
+	int e = check_elephant(x_white_king, y_white_king, x_black_elephant, y_black_elephant);
+	int r = check_rook(x_white_king, y_white_king, x_black_rook, y_black_rook);
 
-	if (f + g == 2) {
+	int s = 0;
+
+	if (e == 0) {
+		e = overlapped_figures("elephant");
+	}
+
+	if (r == 0) {
+		r = overlapped_figures("rook");
+	}
+
+	if (r + e == 2) {
 		cout << "Нет шаха";
 	}
 
+	cout << "\nНажмите любую кнопку чтобы закрыть окно";
+	cin.get();
+	cin.get();
 	return 0;
 }
--- develop/lib.cpp
+++ develop/lib.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 int check_elephant (int x1, int y1, int x2, int y2){
     if ( x1+y1 == x2+y2 || x1-y1 == x2-y2 ){
-        cout << "Ўах от слона\n";
+        return 0;
     }
     else return 1;
-    return 0;
 }
 
 int check_rook (int x1, int y1, int x2, int y2){
     if (x1 == x2 || y1 == y2) {
-        cout << "Ўах от ладьи\n";
+        return 0;
     }
     else return 1;
-    return 0;
 }
